a5a1d650d7dcc6a4b4ea570bef24f68a85b17977
Esau21 <esaumoran1999@gmail.com>
2023-01-26 18:58:47 -0600

    Red neuronal con python machine-learning

---
+++ main.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+def grados():
+ celsius = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
+ fahrenheit = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
+
+ capa1 = tf.keras.layers.Dense(units=1, input_shape=[1])
+ modelo = tf.keras.Sequential([capa1])
+
+ modelo.compile(
+    optimizer=tf.keras.optimizers.Adam(0.1),
+    loss='mean_squared_error'
+ )
+
+ print("Estamos comenzando a entrenar la red")
+ historial = modelo.fit(celsius, fahrenheit, epochs=1000, verbose=False)
+ print("El modelo se ha entrenado con exito.")
+
+
+ plt.xlabel('# Epoca')
+ plt.ylabel('# magnitud de perdida')
+ plt.plot(historial.history["loss"])
+
+ print("Empezemos hacer predicciones")
+ resultado = modelo.predict([100.0])
+ print("El resultado es: ", str(resultado) + " fahrenheit")
+
+ print("variables internas del modelo")
+ print(capa1.get_weights())
+valor =  grados()
+
+
